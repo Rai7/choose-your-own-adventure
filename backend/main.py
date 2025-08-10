fbe4440a1d84ab1957c110495528977c0d1c8cc6
Rai Ramones <rairamones@Rais-MacBook-Pro.local>
2025-08-10 19:41:31 +0700

    first commit5

--- backend/main.py
+++ backend/main.py
@@ -29,5 +29,5 @@ app.include_router(job.router, prefix=settings.API_PREFIX)
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
